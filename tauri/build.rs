32e6932c61e8edcd149e2917b8620aeb6d313f3e
HibernalGlow <3090253564@qq.com>
2025-11-04 19:02:27 +0800

    Merge commit 'eb9c3edd12171499f6d78a89409746b9a5eedf33'

--- tauri/build.rs
+++ tauri/build.rs
@@ -1,3 +1,10 @@
 fn main() {
 	tauri_build::build();
+
+	// Copy dav1d.dll to target/release for bundling
+	let src = "../assets/dll/dav1d.dll";
+	let dest = "../target/release/dav1d.dll";
+	if let Err(e) = std::fs::copy(src, dest) {
+		println!("cargo:warning=Failed to copy dav1d.dll: {}", e);
+	}
 }
